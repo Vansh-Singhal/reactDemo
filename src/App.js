658d6f295d61aea17667adee6e8adeb008a90dea
Vansh Singhal <vansh.singhal@hotmail.com>
2024-08-13 19:17:01 +0530

    BasicFixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Faq from './Faq';
 
 function App() {
+  
   let btnClasses = 'bg-orange-400 text-white border-double border-4 border-sky-500 rotate-90 p-2 ENQUIRE'
   let [modalStatus,setModalState] = useState(false)
-
   return (
     <div className="App">
 
       <button onClick = {()=>setModalState(true)} className={btnClasses}>ENQUIRE</button>
       <div className="modal">
-        <div className={`modalOverlay ${modalStatus ? 'modalShow' : ''  } `} />
+        <div onClick={()=>setModalState(false)} className={`modalOverlay ${modalStatus ? 'modalShow' : ''  } `} />
         <div className={`modalDiv ${modalStatus ? 'modalDivShow' : '' }  `}>
-          <h1 className="text-3xl font-bold m-5">ENQUIRE NOW <span onClick={()=>setModalState(false)}>&times;</span></h1>
+          <h1 className="text-3xl font-bold m-5 ENQUIREHEADING">ENQUIRE NOW <span onClick={()=>setModalState(false)}>&times;</span></h1>
         </div>
       </div>
       
       <Faq/>
+      
     </div>
   );
 }
--- src/Faq.js
+++ src/Faq.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Questions } from './data/data'
 import { useState } from 'react';
 
-export default function () {
-  let [currentID,setCurrentID] = useState(Questions[0].ID)
-  let items = Questions.map((val,i)=>{
+export default function Faq() {
   
+  let [currentID,setCurrentID] = useState(0)
+  let items = Questions.map((val,i)=>{
   let itemData = {val,currentID,setCurrentID}
 
     return(
@@ -26,10 +26,18 @@ export default function () {
 function FaqItem({itemData}){
 
   let {val,currentID,setCurrentID} = itemData;
-
+  let decideVal = () =>{
+    if (currentID===(val.ID)){
+      setCurrentID(0)
+    }
+    else{
+      setCurrentID(val.ID)
+    }
+  }
+  console.log(currentID)
     return(
       <div className='faqItem'>
-        <h2 onClick={()=>setCurrentID(val.ID)} className='text-xl font-medium '>
+        <h2 onClick={decideVal} className='text-xl font-medium '>
           {val.Question}
         </h2>
         <p className = {currentID===val.ID ? "showAns" : "" }>
